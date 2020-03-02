249872d8a9fa9a5b2d50eeab6c1b9a9e40eff79a
EnigmaOffline <lochungtin@gmail.com>
2020-03-02 22:07:03 +0000

    New Filter and Sorter Class

---
+++ Sorter.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Sorter here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+
+public class Sorter
+{
+    /**
+     * Constructor for objects of class Sorter
+     */
+    public Sorter()
+    {
+
+    }
+
+}
