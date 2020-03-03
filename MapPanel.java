94b89f801e50c194cce68cae81400c766492a821
EnigmaOffline <lochungtin@gmail.com>
2020-03-03 11:45:43 +0000

    Fixed Panel Problems

--- MapPanel.java
+++ MapPanel.java
@@ -11,21 +11,17 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Set;
 import javafx.scene.layout.*;
-import javafx.scene.control.*;
 import javafx.scene.shape.*;
 import javafx.scene.paint.*;
 
 public class MapPanel extends Panel
 {
-    private HashMap<String, Borough> list;
-
     /**
      * Constructor for objects of class MapPanel
      */
     public MapPanel()
     {
         super();
-        
     }
 
     private Color getFillColor(int number, int max) {
@@ -34,16 +30,14 @@ public class MapPanel extends Panel
         return Color.rgb(redValue, 255, blueValue);
     }
 
-    public void loadBoroughs() {
-        list = new HashMap<>();
-        list.put("", new Borough ("", "", 10, 10, 10));
-    }
+    public void printTest() {
+        HashMap<String, Integer> boroughs = boroughToPropertyNo(0, 10000000);
+        Set<String> boroughsList = boroughs.keySet();
+        Iterator<String> boroughsIT = boroughsList.iterator();
 
-    public Borough matchBoroughs(String name) {
-        if (list.get(name) != null) {
-            return list.get(name);
+        while (boroughsIT.hasNext()) {
+            System.out.println(boroughsIT.next());
         }
-        return null;
     }
 
     @Override
@@ -55,15 +49,15 @@ public class MapPanel extends Panel
         GridPane gridpane = new GridPane();
 
         int max = Collections.max(boroughs.values());
-        int row = 0;
-        int col = 0;
+
+        loadBoroughs();
 
         while (boroughsIT.hasNext()) {
             String boroughName = boroughsIT.next();
             Borough current = matchBoroughs(boroughName);
             Circle circle = new Circle(current.getRadius());
             circle.relocate(current.getX(), current.getY());
-            circle.setFill(getFillColor(boroughs.get(boroughName), Collections.max(boroughs.values())));
+            circle.setFill(getFillColor(boroughs.get(boroughName), max));
         }
         return gridpane;
     }
