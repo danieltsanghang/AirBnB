0181ea387f2a021f757ac613983ca1f9f22c5ebd
Tsang, Hang (k19027419) <hang.tsang@kcl.ac.uk>
2020-03-05 14:54:16 +0000

    Create listingBorough.java
    created new class

---
+++ src/listingBorough.java
@@ -0,0 +1,58 @@
+import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ComboBox;
+import javafx.beans.value.ChangeListener;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class listingBorough extends Pane{
+
+private BorderPane container = new BorderPane();
+private Scene scene = new Scene(container);
+private ArrayList<Pane> listings = new ArrayList();
+private String name;
+private String abbrevName;
+
+        public listingBorough(String name, String abbrevName, int minPrice, int maxPrice) {
+            this.name = name;
+            this.abbrevName = abbrevName;
+
+            Label sortLabel = new Label("Sort by:");
+            ComboBox sortBox = new ComboBox();
+            sortBox.getItems().addAll("Date","Price","dick size");
+
+            BorderPane topbar = new BorderPane();
+            GridPane sort = new GridPane();
+            sort.add(sortLabel,0,0);
+            sort.add(sortBox, 1,0);
+            topbar.setRight(sort);
+            container.setTop(topbar);
+
+            ScrollPane main = new ScrollPane();
+            VBox vbox = new VBox();
+            main.setContent(vbox);
+            container.setCenter(main);
+        }
+
+
+}
